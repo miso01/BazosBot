0f604cedbad25dbd1451158f904e623263dd8223
miso01 <misosvec01@gmail.com>
2020-07-14 10:17:01 +0200

    config url edit

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 
 class Config:
     SECRET_KEY = "U8mdsl0nwegd6belc"
-    #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:101478@localhost/bazos_bot'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    #SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:101478@localhost/bazos_bot'
